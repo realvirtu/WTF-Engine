c91acabb80158994d698d732d2c7954f3b35f518
realvirtu <103977942+realvirtu@users.noreply.github.com>
2026-09-17 18:57:08 -0400

    Hold shift to play in minimal mode

--- source/funkin/ui/freeplay/FreeplaySubState.hx
+++ source/funkin/ui/freeplay/FreeplaySubState.hx
@@ -299,7 +299,7 @@ class FreeplaySubState extends FunkinSubState
 
 				// Yes it has to be done like this
 				// Um fuck you Flixel
-				final params:PlayStateParams = {song: song, difficulty: difficulty, minimalMode: true};
+				final params:PlayStateParams = {song: song, difficulty: difficulty, minimalMode: FlxG.keys.pressed.SHIFT};
 
 				FlxG.switchState(() -> new PlayState(params));
 			});
